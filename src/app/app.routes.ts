154cfcdd1f9d40d777a439357218d4ed5df89c62
s1mplist <gustavopinheiro.az@outlook.com>
2024-06-15 23:48:26 -0300

    Added Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `home`
+
+  { path: '404', component: PagenotfoundComponent },  // Wildcard route for a 404 page
+  { path: '**',   redirectTo: '/404', pathMatch: 'full' }
+];
